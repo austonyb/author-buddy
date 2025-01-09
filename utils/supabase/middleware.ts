9ef8474afe32199464b7593e4fdb86e10f27d6f2
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2025-01-09 00:54:38 -0700

    moved some things around

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -44,6 +44,10 @@ export const updateSession = async (request: NextRequest) => {
       return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
+    if (request.nextUrl.pathname.startsWith("/tools") && user.error) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
     if (request.nextUrl.pathname === "/" && !user.error) {
       return NextResponse.redirect(new URL("/protected", request.url));
     }
